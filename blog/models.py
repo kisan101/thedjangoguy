8e9fe9388156d58bfd2321071c6d4699335fedbf
kisan101 <kisan.codes@gmail.com>
2020-02-19 22:53:09 +0545

    feat: latest

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,17 @@
+from djang.contrib.auth import get_user_model
 from django.db import models
 
-# class Blog(models.Model):
-	
+
+class Author(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	author_name = CharField()
+
+	def __str__(self):
+		return self.user.username
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	overview = models.TextField()
+	author = models.ForeginKey(Author,on_delete=models.CASCADE)
+
