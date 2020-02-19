8927be9ac37d227765d39d7345f2e1cf4ed994e2
sagarPakhrin <sagarlama826@gmail.com>
2020-02-20 00:36:37 +0545

    - PostListView Created
    - PostDetailView Created
    
    - blog.urls updated
    
    - Class Based View Added

--- blog/admin.py
+++ blog/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
+from .models import Post,Category
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ('id','name')
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at','author','read_time')
--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,33 @@
-from djang.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+import uuid
 from django.db import models
 
-
-class Author(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	author_name = CharField()
-
-	def __str__(self):
-		return self.user.username
-
-
 class Post(models.Model):
-	title = models.CharField(max_length=100)
-	overview = models.TextField()
-	author = models.ForeginKey(Author,on_delete=models.CASCADE)
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
 
+    )
+    title = models.CharField(max_length=60)
+    category = models.ManyToManyField('Category',help_text="Select Categories for this Post")
+    content = models.TextField()
+    banner = models.ImageField(blank=True,null=True)
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    meta_keyworks = models.CharField("Meta Keywords",max_length=255,
+                                     help_text="Comma-delimited set of SEO keywords for meta tag")
+    meta_description = models.CharField("Meta Description",max_length=255,
+                                     help_text="Content for Description Meta Tag")
+    read_time =  models.CharField(max_length=5,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
