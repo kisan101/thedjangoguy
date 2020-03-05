1b09436195db43e03f108a47a883fdd22cc4d072
kisan101 <kisan.codes@gmail.com>
2020-03-05 16:35:47 +0545

    category added

--- codewithkisan/settings.py
+++ codewithkisan/settings.py
@@ -25,11 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #Third Paryo
-
-    #Local
     'blog.apps.BlogConfig',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
